1cf24a49ebbc0431ba5dcb87d70ac36a8e29d8f2
王虹 <1139316015@qq.com>
2021-01-26 22:57:49 +0800

    no message

---
+++ hello-world/src/components/child.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>我是子组件</h3>
+        <p>{{title}}</p>
+        <button @click="modify">点击我修改</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                temp: true,
+                name: ''
+            }
+        },
+        methods: {
+            modify() {
+                this.temp = !this.temp
+                this.name = this.temp? '比尔·盖茨' : '乔布斯'
+                this.$emit('update:title', this.name)
+            }
+        },
+        props: ['title']
+    }
+</script>
+
+<style scoped>
+
+</style>
